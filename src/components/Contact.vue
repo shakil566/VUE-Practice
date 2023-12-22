c32b086998627752652d47fb4afe45568bd2692b
Shakil Hossen <shakilhossen566@gmail.com>
2023-12-22 20:06:40 +0600

    vue-2 project install and router install

---
+++ src/components/Contact.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Contact">
+    <h1>Contact Page </h1> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePage from '@/components/HomePage'
+import About from '@/components/About'
+import Contact from '@/components/Contact'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
